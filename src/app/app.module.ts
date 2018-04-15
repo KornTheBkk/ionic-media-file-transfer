99a5ec3e0922cb96816a6fc908786899d78de518
kornNYPD <kornthebkk@gmail.com>
2018-04-15 11:02:47 +0700

    donwload file success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { Media } from '@ionic-native/media';
+import { FileTransfer} from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Media,
+    File,
+    FileTransfer
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { Media, MediaObject } from '@ionic-native/media';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,68 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  storagePath: string; // path for store file to local device
+  fileUrl: string = 'http://localhost/buddhadasa/storage/sound/mp3/20180414_121003_0328342522.mp3'; // source file
+  fileName: string = 'sound.mp3'; // file name on target
 
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform,
+    private transfer: FileTransfer,
+    private file: File,
+    private media: Media) {
+
+    platform.ready().then(() => {
+
+
+      if (this.platform.is('ios')) {
+        this.storagePath = this.file.documentsDirectory;
+      } else {
+        this.storagePath = this.file.dataDirectory;
+      }
+
+      this.storagePath = this.storagePath + 'buddhadasa/sound/';
+
+    });
+
+  }
+
+  downloadAndOpenSound() {
+
+    this.platform.ready().then(() => {
+
+      let filePath: string;
+
+      filePath = this.storagePath + this.fileName;
+
+      this.file.checkFile(this.storagePath, this.fileName)
+        .then(() => {
+
+          console.log('file found');
+
+        })
+        .catch(error => {
+
+          console.log('file not found : ' + filePath);
+
+          let transfer: FileTransferObject = this.transfer.create();
+
+          transfer.download(this.fileUrl, filePath)
+            .then(entry => {
+
+              let url = entry.toURL();
+              console.log('download success : ' + url);
+              
+            })
+            .catch(error => {
+
+              console.log('download failed : ' + JSON.stringify(error));
+
+            });
+
+        });
+
+    });
   }
 
 }
